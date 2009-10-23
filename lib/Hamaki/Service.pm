a269bfa40cb3f08abdf7e0c39d674829d7e6a7f2
lestrrat <lestrrat+github@gmail.com>
2009-10-23 12:23:50 +0900

    build handlers for each action, not a omnipotent regex

--- lib/Hamaki/Service.pm
+++ lib/Hamaki/Service.pm
@@ -2,6 +2,36 @@ package Hamaki::Service;
 use Moose;
 use namespace::clean -except => qw(meta);
 
+has name => (
+    is => 'ro',
+    isa => 'Str',
+    required => 1
+);
+
+sub get_handlers {
+    my $self = shift;
+    my $name = $self->name;
+    return (
+        {
+            path => qr{^/chat/($name)/poll},
+            handler => 'Hamaki::ChatPollHandler'
+        },
+        {
+            path => qr{/chat/($name)/mxhrpoll},
+            handler => 'Hamaki::ChatMultipartPollHandler',
+        },
+        {
+            path => qr{/chat/($name)/post},
+            handler => 'Hamaki::ChatPostHandler',
+        },
+        {
+            path => qr{/chat/($name)},
+            handler => 'Hamaki::ChatRoomHandler',
+        }
+    )
+}
+
+
 sub start {}
 
 sub BUILD {
